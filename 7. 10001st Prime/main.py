3cff1786ad963263a148e130a335dc6994bf8265
slow-connect <louisdeberlin+github@gmail.com>
2024-01-05 17:00:00 +0100

    acceleration

--- 7. 10001st Prime/main.py
+++ 7. 10001st Prime/main.py
@@ -1,14 +1,14 @@
 def sieve_of_eratosthenes():
-    lst = [(i, True) for i in range(2, 150000)]
+    lst = [True for i in range(150000)]
     cnt = 0
     num = -1
-    for i in range(0, len(lst)):
-        if lst[i][1]:
-            for j in range(i + lst[i][0], len(lst), lst[i][0]):
-                lst[j] = (lst[j][0], False)
+    for i in range(2, len(lst)):
+        if lst[i]:
+            for j in range(2*i, len(lst), i):
+                lst[j] = False
             cnt += 1
             if cnt == 10001:
-                num = lst[i][0]
+                num = i
     return num
 
 def main():
